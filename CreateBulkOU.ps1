1e9cd4e737d42f25de3c097dcc4ec1159bb3bd1c
Pete Stopoulos <99430666+PeteStopoulos@users.noreply.github.com>
2022-05-10 14:36:07 -0400

    Add files via upload
    Create Bulk Organizational Units from CSV

---
+++ CreateBulkOU.ps1
@@ -0,0 +1,17 @@
+﻿# Script to create Bulk AD Organizational Units
+
+$ADOU = import-csv <#C:\Scripts\ou.csv#>    # File path to CSV file of OUs to create
+
+# ou CSV file needs to be laid out as name,path
+
+#---Loops csv file---
+foreach ($ou in $ADOU) {
+    $error.clear() # Clear errors from previous loops
+    $name = $ou.name # Passes name from user created CSV file
+    $path = $ou.path # Passes ou's path from CSV file
+    try {New-ADOrganizationalUnit -Name $name -path $path}
+    catch {write-warning "An error occurred creating OU $name"} # Catches any errors and displays a warning
+    if (!$error) {
+        Write-Host "OU $name has been created!" -ForegroundColor Green
+    }
+}
